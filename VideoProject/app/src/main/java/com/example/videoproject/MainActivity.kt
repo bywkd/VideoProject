583ccdcf1fe6f7e6280c2d92e64b0eba4874f1cc
DESKTOP-U18BGG7\wonwoo <cd100go@google.com>
2019-06-23 19:27:36 +0900

    video-0004: 커밋 꼬여서 dev 병합이 안되서 다시 올림

--- VideoProject/app/src/main/java/com/example/videoproject/MainActivity.kt
+++ VideoProject/app/src/main/java/com/example/videoproject/MainActivity.kt
@@ -8,7 +8,6 @@ import com.example.videoproject.fragment.SecondFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
